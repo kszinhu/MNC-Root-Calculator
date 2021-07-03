6e52c0451e722ace0e85385f968a285da9b64042
Cassiano - Manjaro <rodrigues.cassiano2007@gmail.com>
2021-07-03 00:09:26 -0300

    fix: refactoring methods

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,3 +1,31 @@
+function formattingExpression(input) {
+  const output = input
+    .replace(/sen|sin/gi, "sin")
+    .replace(/cos/gi, "cos")
+    .replace(/tg|tan/gi, "tan")
+    .replace(/\^/gi, "**")
+    .replace(/pi/gi, "Math.PI")
+    .replace(/\log\D/gi, "log10(")
+    .replace(/\ln/gi, "log")
+    .replace(/\e/gi, "Math.E");
+  return output;
+}
+
+function calculateFirstDerivative(input, x, epsilon) {
+  let h = 1000 * epsilon;
+  p = (evaluate(input, { x: x + h }) - evaluate(input, { x: x - h })) / (2 * h);
+
+  for (let index = 0; index < 10; index++) {
+    let q = p;
+    h /= 2;
+
+    p =
+      (evaluate(input, { x: x + h }) - evaluate(input, { x: x - h })) / (2 * h);
+    if (Math.abs(p - q) < epsilon) break;
+  }
+  return p;
+}
+
 //  Eval com asteroídes
 evaluate = (input, variables) => {
   with (variables) with (Math) return eval(input);
@@ -5,6 +33,7 @@ evaluate = (input, variables) => {
 
 // parametros (f(x), a, b, delta)
 BuscaUniforme = (input, a, b, delta, cont = 0) => {
+  input = formattingExpression(input);
   for (let x = a; x < b; x += delta) {
     let p = x;
     let q = p + delta;
@@ -28,6 +57,7 @@ BuscaUniforme = (input, a, b, delta, cont = 0) => {
  */
 
 Bisseccao = (input, a, b, epsilon) => {
+  input = formattingExpression(input);
   while (Math.abs(b - a) / 2 > epsilon) {
     xi = (a + b) / 2;
     if (evaluate(input, { x: xi }) == 0) {
@@ -41,7 +71,7 @@ Bisseccao = (input, a, b, epsilon) => {
       }
     }
   }
-  console.log(`Valor da Raiz é: ${xi}`);
+  return xi;
 };
 
 /** Método das Cordas
@@ -50,6 +80,7 @@ Bisseccao = (input, a, b, epsilon) => {
  */
 
 Cordas = (input, a, b, epsilon) => {
+  input = formattingExpression(input);
   while (Math.abs(b - a) > epsilon) {
     xi =
       (a * Math.abs(evaluate(input, { x: b })) +
@@ -67,41 +98,57 @@ Cordas = (input, a, b, epsilon) => {
       }
     }
   }
-  console.log(`Valor da Raiz é: ${xi}`);
+  return xi;
 };
 
 //  Método de Newton
 
 Newton = (input, a, epsilon, maxIt) => {
+  input = formattingExpression(input);
   k = 0;
   p = a;
   do {
     k = k + 1;
     q = p;
-    if(calculateFirstDerivative(p, x, epsilon) != 0) {
-    p = p - (evaluate(input, { x: p }) / calculateFirstDerivative(p, x, epsilon));
-    } else if(calculateFirstDerivative(p, x, epsilon) == 0) {  // se f'(x) = 0, reinicie com outro p ou recalcule f' comp+epsilon
-      p = p - (evaluate(input, { x: p }) / calculateFirstDerivative(p+epsilon, x, epsilon)); 
-    }
-  } while((Math.abs(p-q) >= epsilon) && (k != maxIt));
+    p =
+      p -
+      evaluate(input, { x: p }) / calculateFirstDerivative(input, p, epsilon);
+  } while (Math.abs(p - q) >= epsilon && k != maxIt);
+  return p;
 };
 
 //  Método de Newton Modificado
 
 modifiedNewton = (input, a, epsilon, maxIt) => {
+  input = formattingExpression(input);
   k = 0;
   p = a;
+  debugger;
   do {
-    if((k % 5) == 0) { // mod 5
-      if(calculateFirstDerivative(p, x, epsilon) == 0) {  // se f'(x) = 0, reinicie com outro p ou recalcule f' com p+epsilon
-        p = p - (evaluate(input, { x: p }) / calculateFirstDerivative(p+epsilon, x, epsilon));
-      }else{
-        d = calculateFirstDerivative(p, x, epsilon);
-      } 
-    } else {
-      k = k+1;
-      q = p;
-      p = p - ((evaluate(input, { x: p }) / d));
+    if (k % 5 == 0) {
+      // mod 5
+      if (calculateFirstDerivative(input, p, epsilon) == 0) {
+        // se f'(x) = 0, reinicie com outro p ou recalcule f' com p+epsilon
+
+        p =
+          p -
+          evaluate(input, { x: p }) /
+            calculateFirstDerivative(input, p + epsilon, epsilon);
+        debugger;
+      } else {
+        d = calculateFirstDerivative(input, p, epsilon);
+        debugger;
+      }
     }
-  } while((Math.abs(p-q) >= epsilon) && (k != maxIt));
+    k = k + 1;
+    q = p;
+    p = p - evaluate(input, { x: p }) / d;
+    debugger;
+  } while (Math.abs(p - q) >= epsilon && k != maxIt);
+  return p;
 };
+
+/** Método das Cordas Modificado
+ * Quando se tem certeza que existe uma raiz no intervalo de busca e existe apenas uma,
+ *  caso contrário método poderá falhar
+ */
