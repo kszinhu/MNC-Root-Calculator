89db40e2b72e4dcb1f837c70ffbd847b9184bc44
Cassiano - Manjaro <rodrigues.cassiano2007@gmail.com>
2021-07-05 21:44:26 -0300

    feat adding js

--- src/js/methods.js
+++ src/js/methods.js
@@ -1,8 +1,8 @@
-/** Busca Uniforme
+/** Separa com Busca Uniforme
  * Encontra indício de raízes no intervalo
  */
 
-BuscaUniforme = (input, a, b, delta, cont = 0) => {
+const BuscaUniforme = (input, a, b, delta, cont = 0) => {
   let arrayA = [];
   let arrayB = [];
 
@@ -13,7 +13,6 @@ BuscaUniforme = (input, a, b, delta, cont = 0) => {
       Math.sign(evaluate(input, { x: p })) !=
       Math.sign(evaluate(input, { x: q }))
     ) {
-      console.log(`Achou intervalo que há uma raíz ${++cont}`);
       arrayA.push(p);
       arrayB.push(q);
     } else if (
@@ -26,17 +25,35 @@ BuscaUniforme = (input, a, b, delta, cont = 0) => {
   return { a: arrayA, b: arrayB };
 };
 
+/** Primeira Raíz com Busca Uniforme
+ *  Encontra a primeira raíz no intervalo e calcula a raíz
+ */
+
+const RootBuscaUniforme = (input, a, b, epsilon, cont = 0) => {
+  let delta = (b - a) / 1000;
+  var fpp = evaluate(input, { x: a });
+  for (var p = a; p < b; p += epsilon) {
+    let fp = evaluate(input, { x: p });
+    if (Math.sign(fp) != Math.sign(fpp) && Math.abs(fp) <= epsilon) {
+      return { root: p - delta, cont: cont };
+    }
+    fpp = fp;
+    cont++;
+  }
+  return false;
+};
+
 /** Método da Divisão ao Meio
  * Quando se tem certeza que existe uma raiz no intervalo de busca e existe apenas uma,
  *  caso contrário método poderá falhar
  */
 
-Bisseccao = (input, a, b, epsilon) => {
-  input = formattingExpression(input);
+const Bisseccao = (input, a, b, epsilon, cont = 0) => {
+  let xi;
   while (Math.abs(b - a) / 2 > epsilon) {
     xi = (a + b) / 2;
     if (evaluate(input, { x: xi }) == 0) {
-      console.log(`Raíz é: ${xi}`); // Certeza
+      // Certeza que é a raíz
       break;
     } else {
       if (evaluate(input, { x: a }) * evaluate(input, { x: xi }) < 0) {
@@ -45,8 +62,9 @@ Bisseccao = (input, a, b, epsilon) => {
         a = xi;
       }
     }
+    cont++;
   }
-  return xi;
+  return { root: xi, cont: cont };
 };
 
 /** Método das Cordas
@@ -54,8 +72,8 @@ Bisseccao = (input, a, b, epsilon) => {
  *  caso contrário método poderá falhar
  */
 
-Cordas = (input, a, b, epsilon) => {
-  input = formattingExpression(input);
+const Cordas = (input, a, b, epsilon, cont = 0) => {
+  let xi;
   while (Math.abs(b - a) > epsilon) {
     xi =
       (a * Math.abs(evaluate(input, { x: b })) +
@@ -63,7 +81,7 @@ Cordas = (input, a, b, epsilon) => {
       (Math.abs(evaluate(input, { x: b })) +
         Math.abs(evaluate(input, { x: a })));
     if (evaluate(input, { x: xi }) == 0) {
-      console.log(`Raíz é: ${xi}`); // Certeza
+      // Certeza que é a raíz
       break;
     } else {
       if (evaluate(input, { x: a }) * evaluate(input, { x: xi }) < 0) {
@@ -72,8 +90,9 @@ Cordas = (input, a, b, epsilon) => {
         a = xi;
       }
     }
+    cont++;
   }
-  return xi;
+  return { root: xi, cont: cont };
 };
 
 /** Método de Newton
@@ -81,10 +100,9 @@ Cordas = (input, a, b, epsilon) => {
  *  caso contrário método poderá falhar
  */
 
-Newton = (input, a, epsilon, maxIt) => {
-  input = formattingExpression(input);
-  k = 0;
-  p = a;
+const Newton = (input, a, epsilon, maxIt) => {
+  let k = 0;
+  let p = a;
   do {
     k = k + 1;
     q = p;
@@ -92,7 +110,7 @@ Newton = (input, a, epsilon, maxIt) => {
       p -
       evaluate(input, { x: p }) / calculateFirstDerivative(input, p, epsilon);
   } while (Math.abs(p - q) >= epsilon && k != maxIt);
-  return p;
+  return { root: p, cont: k };
 };
 
 /** Método de Newton Modificado
@@ -100,36 +118,64 @@ Newton = (input, a, epsilon, maxIt) => {
  *  caso contrário método poderá falhar
  */
 
-modifiedNewton = (input, a, epsilon, maxIt) => {
-  input = formattingExpression(input);
-  k = 0;
-  p = a;
-  debugger;
+const modifiedNewton = (input, a, epsilon, maxIt) => {
+  let k = 0;
+  let p = a;
   do {
     if (k % 5 == 0) {
       // mod 5
       if (calculateFirstDerivative(input, p, epsilon) == 0) {
         // se f'(x) = 0, reinicie com outro p ou recalcule f' com p+epsilon
-
         p =
           p -
           evaluate(input, { x: p }) /
             calculateFirstDerivative(input, p + epsilon, epsilon);
-        debugger;
       } else {
         d = calculateFirstDerivative(input, p, epsilon);
-        debugger;
       }
     }
     k = k + 1;
     q = p;
     p = p - evaluate(input, { x: p }) / d;
-    debugger;
   } while (Math.abs(p - q) >= epsilon && k != maxIt);
-  return p;
+  return { root: p, cont: k };
 };
 
 /** Método das Cordas Modificado
  * Quando se tem certeza que existe uma raiz no intervalo de busca e existe apenas uma,
  *  caso contrário método poderá falhar
  */
+
+const modifiedRope = (input, a, b, epsilon) => {
+  let k = 1;
+  let modA,
+    modB = 0;
+  var fa, fb, fp, b;
+  do {
+    fa = evaluate(input, { x: a });
+    fb = evaluate(input, { x: b });
+    if (modA > 3) {
+      p = ((a * fb) / 2 - b * fa) / (fb / 2 - fa);
+    } else if (modB > 3) {
+      p = (a * fb - (b * fa) / 2) / (fb - fa / 2);
+    } else {
+      p = (a * fb - b * fa) / (fb - fa);
+    }
+
+    fp = evaluate(input, { x: p });
+
+    if (Math.sign(fa) != Math.sign(fb)) {
+      a = p;
+      modA = 0;
+      modB++;
+    } else {
+      b = p;
+      modB = 0;
+      modA++;
+    }
+    k++;
+  } while (Math.abs(b - a) > epsilon && fp > epsilon);
+  if (!(p == a - 1)) {
+    return { root: p, cont: k };
+  }
+};
